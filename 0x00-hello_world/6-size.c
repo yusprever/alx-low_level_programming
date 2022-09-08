62f7885cd24a74fb38172444079ba63f0c33104c
yusprever <kiserotobin12@gamil.com>
2022-09-08 04:33:30 -0700

    temps

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+        printf("Size of a char: %lu\n",sizeof(char));
+        printf("Size of an int: %lu\n",sizeof(int));
+        printf("Size of a long int: %lu\n",sizeof(long));
+        printf("Size of a long long int: %lu\n",sizeof(long long);
+        printf("Size of a float: %lu\n",sizeof(float));
+        return (0);
+}
