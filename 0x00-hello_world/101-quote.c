613a0cc7d4c6699332642cf58815fa064077ca03
yusprever <kiserotobin12@gamil.com>
2022-09-08 05:51:42 -0700

    final

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main displays a string to the user
+ * main -Entry point
+ *
+ * Return:Always 1 for success
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
