d617427f60307803b674bea0a2c0747f1ac5843c
yusprever <kiserotobin12@gamil.com>
2022-09-14 04:48:49 -0700

    last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ * @c: int
+ * description - return last digit
+ * Return: int
+ */
+
+int print_last_digit(int c)
+{
+	int q = c % 10;
+
+	return (q);
+}
