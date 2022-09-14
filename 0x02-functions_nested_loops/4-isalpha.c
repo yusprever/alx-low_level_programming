74fae9f6e4bba283b8997008ebe29fd62ec65b8b
yusprever <kiserotobin12@gamil.com>
2022-09-14 04:23:48 -0700

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isalpha: any letter
+ * @c: variable
+ * description : check letter
+ * Return: int
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
