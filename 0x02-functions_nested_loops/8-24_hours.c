1bc018f17287387ce0de46a330d3386e9485eaf6
yusprever <kiserotobin12@gamil.com>
2022-09-14 05:05:28 -0700

    soundtrack

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * void jack_bauer - print every minute
+ * @void: no value
+ * description - print every minute off the day
+ * Return: null
+ */
+
+void jack_bauer(void)
+{
+	int a, b;
+
+	for (a = 0; a < 24; a++)
+        {
+
+                for (b = 0; b < 60; b++)
+                {
+
+                        _putchar((a / 10) + '0');
+
+                        _putchar((a % 10) + '0');
+
+                        _putchar(':');
+
+                        _putchar((b / 10) + '0');
+
+                        _putchar((b % 10) + '0');
+
+                        _putchar('\n');
+		}
+	}
+}
