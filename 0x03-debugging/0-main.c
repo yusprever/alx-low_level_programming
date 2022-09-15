34c1df3e7691a3671b8834e418144920741d8738
yusprever <kiserotobin12@gamil.com>
2022-09-15 02:16:16 -0700

    testing

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-* main - tests function positiv_or_negative
+* main - tests function that prints if integer is +ve/-ve
 * Return: 0
 */
 int main(void)
 {
 	int i;
 
-	i = 98;
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
