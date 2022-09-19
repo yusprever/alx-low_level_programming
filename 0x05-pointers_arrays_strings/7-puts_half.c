dd46fe31ee9d28f5a54919db8e161a4981f89ad8
yusprever <kiserotobin12@gamil.com>
2022-09-19 06:07:04 -0700

    just a half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of the string
+ * @str: random string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int size = 0;
+	int stop;
+	
+
+	while (*(str +size))
+	{
+		size++;
+	}
+	
+	if (size % 2 == 0)
+	{
+		stop = size / 2;
+	}
+	else
+	{
+		stop = (size - 1) / 2;
+	}
+
+	while (stop < size)
+	{
+		_putchar(*(str + stop));
+		stop++;
+	}
+	_putchar('\n');
+
+}
