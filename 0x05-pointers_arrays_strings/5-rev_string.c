f7c94965e05277006aa476b32747c1c0b6a1770d
yusprever <kiserotobin12@gamil.com>
2022-09-19 05:25:32 -0700

    reversed pointer

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses string
+ * @s: string to be reversed
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+        int len = 0;
+	int i = 0;
+	char store[1000];
+	
+        while (*(s + len))
+        {
+		*(store + len) = *(s + len);
+                len++;
+        }
+
+        len--;
+
+        while (len >= 0)
+        {
+                *(s + i) = *(store + len);
+		i++;
+                len--;
+        }
+	
+
+       
+}
