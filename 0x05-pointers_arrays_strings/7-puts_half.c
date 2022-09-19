b8a104eddc6d092e7a039c75dc4e3fbc763a430a
yusprever <kiserotobin12@gamil.com>
2022-09-19 07:51:48 -0700

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,19 +10,17 @@ void puts_half(char *str)
 {
 	int size = 0;
 	int stop;
-	
 
 	while (*(str +size))
 	{
 		size++;
 	}
-	
+
 	stop = size / 2;
 	if (size % 2)
 	{
 		stop += 1;
 	}
-	
 	while (stop < size)
 	{
 		_putchar(*(str + stop));
