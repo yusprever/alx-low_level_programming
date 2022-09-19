19c1924b5b7b0e6dd1563908ac9f152a0b8da032
yusprever <kiserotobin12@gamil.com>
2022-09-19 05:41:04 -0700

    even

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - skips by 2
+ * @str: string
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while ( *(str + i))
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(*(str + i ));
+		}
+		i++;
+	}
+	_putchar('\n');
+}
