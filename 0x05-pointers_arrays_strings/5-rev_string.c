9e9928bee6c7b9faaa9a6dfe47c5b4b03d895f14
yusprever <kiserotobin12@gamil.com>
2022-09-19 05:29:32 -0700

    reversed pointer

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,25 +8,22 @@
  */
 void rev_string(char *s)
 {
-        int len = 0;
+	int len = 0;
 	int i = 0;
 	char store[1000];
 	
-        while (*(s + len))
-        {
+	while (*(s + len))
+	{
 		*(store + len) = *(s + len);
-                len++;
-        }
+		len++;
+	}
 
-        len--;
+	len--;
 
-        while (len >= 0)
-        {
-                *(s + i) = *(store + len);
+	while (len >= 0)
+	{
+		*(s + i) = *(store + len);
 		i++;
-                len--;
-        }
-	
-
-       
+		len--;
+	}
 }
