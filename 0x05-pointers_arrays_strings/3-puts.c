4bb27e604c78e144b259064c53e75b36d9df771d
yusprever <kiserotobin12@gamil.com>
2022-09-19 03:49:31 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+include "main.h"
+
+/**
+ * _puts - prints a string in stdout
+ * @str: string to print
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	_putchar("%d \n",str);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,4 +4,5 @@ extern int _putchar(char c);
 extern void reset_to_98(int *n);
 extern void swap_int(int *a, int *b);
 extern int _strlen(char *s);
+extern void _puts(char *str);
 #endif
