2efaa1f95cf8d210a6d07925951487a0d3ef4be1
yusprever <kiserotobin12@gamil.com>
2022-09-22 05:45:50 -0700

    my own copy function

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies contents of str 2 to str 1
+ * @dest: string 1
+ * @src: string 2
+ * @n: number of bytes to append
+ *
+ * Return: char dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+
+	int len = 0;
+
+	while (len < n)
+	{
+		dest[len] = *(src + len);
+		len++;
+	}
+	return (dest);
+}
