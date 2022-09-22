300a1d7a7aa8ef64da41524e7b61b4e6381fc7b5
yusprever <kiserotobin12@gamil.com>
2022-09-22 06:56:21 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,29 +10,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-
-	int len = 0;
-	int len2 = 0;
-
-	while (*(s1 + len))
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		len++;
-	}
-	while (*(s2 + len2))
-	{
-		len2++;
+		s1++;
+		s2++;
 	}
 
-	if (len > len2)
-	{
-		return (15);
-	}
-	else if (len < len2)
-	{
-		return (-15);
-	}
-	else
-	{
-		return (0);
-	}
+	return (*s1 - *s2);
 }
