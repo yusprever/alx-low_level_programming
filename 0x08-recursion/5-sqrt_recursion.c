76421cd8a72a9646bc0aa3e1d4d92a7332709dc4
yusprever <kiserotobin12@gamil.com>
2022-09-28 07:14:48 -0700

    prime numbers

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,3 @@
-#include "main.h"
 
 /**
  *  * _sqrt_recursion - main funct
@@ -7,22 +6,4 @@
  *     */
 int _sqrt_recursion(int n)
 {
-		return (_sqrt(n, 1));
-}
-
-/**
- *  * _sqrt - _sqrt_recursion
- *   * @n: integer paramtr
- *    * @i: integer parameter
- *     * Return: sqrt
- *      */
-int _sqrt(int n, int i)
-{
-		if (n < 0)
-					return (-1);
-			if ((i * i) > n)
-						return (-1);
-				if (i * i == n)
-							return (i);
-					return (_sqrt(n, i + 1));
 }
