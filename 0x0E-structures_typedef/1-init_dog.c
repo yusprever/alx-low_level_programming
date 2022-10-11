bb5899eb8e16fbe6b15fcadf3086c78df5b55a89
yusprever <kiserotobin12@gamil.com>
2022-10-11 03:39:55 -0700

    task1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - initialize dog struct
+ * @d: object of struct
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
