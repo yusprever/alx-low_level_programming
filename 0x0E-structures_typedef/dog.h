e89a4d1913cffbf0f67d1caadea13c2b04d502b9
yusprever <kiserotobin12@gamil.com>
2022-10-11 03:23:46 -0700

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H_
 #define DOG_H_
 
+/**
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ */
 struct dog
 {
 	char *name;
